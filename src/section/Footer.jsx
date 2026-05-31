bf06bea5d3204b5d240f41889b9ca4cad9b08ce3
alfario207 <alfariolilhuda@gmail.com>
2026-05-31 15:33:00 +0700

    mini update

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="flex justify-center">
-            <p className="dark:text-[#E4E4E7] text-sm">© 2025 Sarah Chen. All rights reserved.</p>
+            <p className="dark:text-[#E4E4E7] text-sm">© 2025 Sarah Chen. All rights reserved (created by Alfario).</p>
         </div>
     )
 }
